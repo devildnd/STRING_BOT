9f0c4f6f612b5b4c4892711bb6c9eee2a50607d3
devildnd <134326031+devildnd@users.noreply.github.com>
2024-07-03 16:07:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #❖________①_______❖_______#
-API_ID = int(getenv("API_ID", 18688563))
+API_ID = int(getenv("18688563")
 
 #❖________②_______❖_______#
-API_HASH = getenv("API_HASH", 48315c25585c23eee6b1f346aad17a68)
+API_HASH = getenv("48315c25585c23eee6b1f346aad17a68")
 
 #❖________③_______❖_______#
-BOT_TOKEN = getenv("BOT_TOKEN", 5935941442:AAEIZGT8QgfVBY35eX4znwSag0uN9sniskk)
+BOT_TOKEN = getenv("5935941442:AAEIZGT8QgfVBY35eX4znwSag0uN9sniskk")
 
 #❖________④_______❖_______#
-OWNER_ID = int(getenv("OWNER_ID", 5562877717))
+OWNER_ID = int(getenv("5562877717")
 
 #❖________⑤_______❖_______#
-MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://lnkgeevr:lnkgeevr@cluster0.bp33u5p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
-
+MONGO_DB_URI = getenv("mongodb+srv://lnkgeevr:lnkgeevr@cluster0.bp33u5p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 #❖________⑥_______❖_______#
-MUST_JOIN = getenv("MUST_JOIN", rockstar_remix)
+MUST_JOIN = getenv("rockstar_remix")
