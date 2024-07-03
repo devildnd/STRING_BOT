bc1f09a2041e78b0611eed22420f49c176dd9cbe
devildnd <134326031+devildnd@users.noreply.github.com>
2024-07-03 17:22:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,18 +8,18 @@ from pyrogram import filters
 load_dotenv()
 
 #❖________①_______❖_______#
-API_ID = 18688563
+API_ID = '18688563'
 
 #❖________②_______❖_______#
 API_HASH = '48315c25585c23eee6b1f346aad17a68'
 
 #❖________③_______❖_______#
-BOT_TOKEN = 5935941442:AAEIZGT8QgfVBY35eX4znwSag0uN9sniskk
+BOT_TOKEN = '5935941442:AAEIZGT8QgfVBY35eX4znwSag0uN9sniskk'
 
 #❖________④_______❖_______#
-OWNER_ID = 5562877717
+OWNER_ID = '5562877717'
 
 #❖________⑤_______❖_______#
-MONGO_DB_URI = mongodb+srv://lnkgeevr:lnkgeevr@cluster0.bp33u5p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+MONGO_DB_URI = 'mongodb+srv://lnkgeevr:lnkgeevr@cluster0.bp33u5p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 #❖________⑥_______❖_______#
-MUST_JOIN = rockstar_remix
+MUST_JOIN = 'rockstar_remix'
